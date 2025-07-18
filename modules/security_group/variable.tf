58460fdb040283422aa29ae9075105d203c4a97f
Kishore10091992 <kishorek1009@yahoo.com>
2025-07-18 18:43:14 +0530

    updated

---
+++ modules/security_group/variable.tf
@@ -0,0 +1,29 @@
+variable "vpc_id" {
+ description = "id for vpc"
+ type = string
+}
+
+variable "from_port" {
+ description = "sg from port"
+ type = string
+}
+
+variable "to_port" {
+ description = "sg to port"
+ type = string
+}
+
+variable "protocol" {
+ description = "sg protocol"
+ type = string
+}
+
+variable "cidr_blocks" {
+ description = "sg cidr block"
+ type = string
+}
+
+variable "tags" {
+ description = "sg tags"
+ type = map(string)
+}
