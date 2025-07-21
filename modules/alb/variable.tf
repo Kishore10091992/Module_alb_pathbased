f729268e09f78efac233704647d4a52c55de3fd7
Kishore Kumar <kishorek1009@yahoo.com>
2025-07-21 10:48:08 +0530

    Update variable.tf

---
+++ modules/alb/variable.tf
@@ -0,0 +1,14 @@
+variable "internal" {
+ description = "lb internal"
+ type = bool
+}
+
+variable "lb_type" {
+ description = "lb type"
+ type = string
+}
+
+variable "tags" {
+ description = "lb tag"
+ type = string
+}
