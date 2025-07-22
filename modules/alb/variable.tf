4fc7c099b9c3bbb0968b90d178c2e3bcb7b5fa93
Kishore Kumar <kishorek1009@yahoo.com>
2025-07-22 10:51:19 +0530

    Update variable.tf

--- modules/alb/variable.tf
+++ modules/alb/variable.tf
@@ -12,3 +12,33 @@ variable "tags" {
  description = "lb tag"
  type = string
 }
+
+variable "sg_id" {
+ description = "security group id"
+ type = string
+}
+
+variable "subnet-1_id" {
+ description = "subnet 1 id"
+ type= string
+}
+
+variable "subnet-2_id" {
+ description = "subnet 2 id"
+ type = string
+}
+
+variable "vpc_id" {
+ description = "vpcid"
+ type = string
+}
+
+variable "app-1_id" {
+ description = "app 1 ec2 id"
+ type = string
+}
+
+variable "app-2_id" {
+ description = "app 2 ec2 id"
+ type = string
+}
