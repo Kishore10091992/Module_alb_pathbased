c2f5a3ebaac3bd38d92485b5a6c6fc681d8fd352
Kishore Kumar <kishorek1009@yahoo.com>
2025-07-23 11:06:18 +0530

    Update main.tf

--- modules/app-2/main.tf
+++ modules/app-2/main.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "app-2_ec2" {
   network_interface_id = aws_network_interface.app-2_nic.id
  }
 
- user_data = var.app_1_userdata
+ user_data = var.app_2_userdata
 
  tags = var.tags
 }
