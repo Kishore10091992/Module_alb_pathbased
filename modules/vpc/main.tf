59c8e76967dc144c511abc59a7d5871e83d0a805
Kishore Kumar <kishorek1009@yahoo.com>
2025-07-23 17:31:54 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -45,12 +45,12 @@ resource "aws_route_table_association" "sub-2_rt_asso" {
 
 resource "aws_network_interface" "app-1_nic" {
  subnet_id = aws_subnet.main_subnet-1.id
- security_groups = [aws_security_group.main_sg.id]
+ security_groups = [var.sg_id]
 }
 
 resource "aws_network_interface" "app-2_nic" {
  subnet_id = aws_subnet.main_subnet-2.id
- security_groups = [aws_security_group.main_sg.id]
+ security_groups = [var.sg_id]
 }
 
 resource "aws_eip" "app-1_eip" {
